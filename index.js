61f3caa5e1d541990a97ece8343a1f825acd1d9a
senaida olivares <senaida.olivares@yahoo.com>
2023-03-28 17:08:47 -0400

    commit tree 8a2064d7a75c517ad1eb0cc96a8d6bc0f129fceb

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Write your solution in this file!
+const employee = {
+    name: "Lily",
+    streetAddress: "97 Street",
+};
+
+function updateEmployeeWithKeyAndValue () {
+    return {
+        ...employee,
+        ["name"]: "Sam",
+        ["streetAddress"]: "11 Broadway",
+    };
+}
+
+const employeeUpdatedAddress = updateEmployeeWithKeyAndValue(employee, "streetAddress", "11 Broadway");
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.streetAddress = "12 Broadway";
+    return employee;
+  }
+
+const destructiveChangeAddress = destructivelyUpdateEmployeeWithKeyAndValue( employee, "streetAddress", "12 Broadway");
+
+beforeEach(function () {
+    for (const key in employee) {
+        delete employee [key];
+    }
+    employee.name ='Sam';
+});
+
+const newEmployee = [...employee];
+function deleteFromEmployeeByKey () {
+    return {
+        newEmployee,
+        delete : employee["name"],
+    }
+};
+
+const copyOfNewEmployee = deleteFromEmployeeWithKey(employee, "name");
+
+beforeEach(function () {
+    for (const key in employee) {
+        delete employee [key];
+    }
+    employee.name ='Sam';
+});
+
+function destructivelyDeleteFromEmployeeByKey() {
+   delete employee["name"];
+   return employee;
+};
+
+let newEmployeeName = destructivelyDeleteFromEmployeeByKey(employee, "name");
